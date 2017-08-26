e1459e86296087cfd71e80d4d15e7fd633d28426
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-08-26 01:12:55 -0500

    Solve tail and setHead

--- src/main/scala/org/fredy/fpinscala/Chapter3.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter3.scala
@@ -4,4 +4,20 @@ object Chapter3 {
   sealed trait List[+A]
   case object Nil extends List[Nothing]
   case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def apply[A](as: A*): List[A] = // Variadic function syntax
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+
+    def tail[A](l: List[A]): List[A] = l match {
+      case Nil => sys.error("tail of empty list")
+      case Cons(_, xs) => xs
+    }
+
+    def setHead[A](l: List[A], h: A): List[A] = l match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(h, xs)
+    }
+  }
 }
--- src/test/scala/org/fredy/fpinscala/Chapter3Suite.scala
+++ src/test/scala/org/fredy/fpinscala/Chapter3Suite.scala
@@ -3,4 +3,13 @@ package org.fredy.fpinscala
 import org.scalatest.FunSuite
 
 class Chapter3Suite extends FunSuite {
+  test("tail") {
+    val list = Chapter3.List.tail(Chapter3.List(1, 2, 3, 4, 5))
+    assert(Chapter3.List(2, 3, 4, 5) == list)
+  }
+
+  test("setHead") {
+    val list = Chapter3.List.setHead(Chapter3.List(1, 2, 3, 4, 5), 10)
+    assert(Chapter3.List(10, 2, 3, 4, 5) == list)
+  }
 }
