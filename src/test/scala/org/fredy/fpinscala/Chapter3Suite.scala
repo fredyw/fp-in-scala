b4d2d69ec23e3ed3e1278007028aedb387082bf4
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-02 17:13:43 -0500

    Update test cases

--- src/test/scala/org/fredy/fpinscala/Chapter3Suite.scala
+++ src/test/scala/org/fredy/fpinscala/Chapter3Suite.scala
@@ -3,22 +3,22 @@ package org.fredy.fpinscala
 import org.scalatest.FunSuite
 
 class Chapter3Suite extends FunSuite {
-  test("tail") {
+  test("List.tail") {
     val list = Chapter3.List.tail(Chapter3.List(1, 2, 3, 4, 5))
     assert(Chapter3.List(2, 3, 4, 5) == list)
   }
 
-  test("setHead") {
+  test("List.setHead") {
     val list = Chapter3.List.setHead(Chapter3.List(1, 2, 3, 4, 5), 10)
     assert(Chapter3.List(10, 2, 3, 4, 5) == list)
   }
 
-  test("drop") {
+  test("List.drop") {
     val list = Chapter3.List.drop(Chapter3.List(1, 2, 3, 4, 5), 2)
     assert(Chapter3.List(3, 4, 5) == list)
   }
 
-  test("dropWhile") {
+  test("List.dropWhile") {
     val list1 = Chapter3.List.dropWhile(Chapter3.List(1, 2, 3, 4, 5), (x: Int) => x < 3)
     assert(Chapter3.List(3, 4, 5) == list1)
 
@@ -26,7 +26,7 @@ class Chapter3Suite extends FunSuite {
     assert(Chapter3.List(4, 5) == list2)
   }
 
-  test("init") {
+  test("List.init") {
     val list1 = Chapter3.List.init(Chapter3.List(1, 2, 3, 4, 5))
     assert(Chapter3.List(1, 2, 3, 4) == list1)
 
@@ -34,7 +34,7 @@ class Chapter3Suite extends FunSuite {
     assert(Chapter3.List(1) == list2)
   }
 
-  test("length") {
+  test("List.length") {
     val list1 = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.length(list1) == 5)
 
@@ -42,74 +42,74 @@ class Chapter3Suite extends FunSuite {
     assert(Chapter3.List.length(list2) == 0)
   }
 
-  test("foldLeft") {
+  test("List.foldLeft") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.foldLeft(list, 0)((b, a) => b + a) == 15)
   }
 
-  test("foldRight") {
+  test("List.foldRight") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.foldRight(list, 0)((a, b) => a + b) == 15)
   }
 
-  test("sum") {
+  test("List.sum") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.sum(list) == 15)
   }
 
-  test("product") {
+  test("List.product") {
     val list = Chapter3.List(1.0, 2.0, 3.0, 4.0, 5.0)
     assert(Chapter3.List.product(list) == 120)
   }
 
-  test("reverse") {
+  test("List.reverse") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.reverse(list) == Chapter3.List(5, 4, 3, 2, 1))
   }
 
-  test("append") {
+  test("List.append") {
     val list1 = Chapter3.List(1, 2)
     val list2 = Chapter3.List(3, 4, 5)
     assert(Chapter3.List.append(list1, list2) == Chapter3.List(1, 2, 3, 4, 5))
   }
 
-  test("concat") {
+  test("List.concat") {
     val list = Chapter3.List(Chapter3.List(1, 2), Chapter3.List(3), Chapter3.List(4, 5))
     assert(Chapter3.List.concat(list) == Chapter3.List(1, 2, 3, 4, 5))
   }
 
-  test("add") {
+  test("List.add") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.add(list) == Chapter3.List(2, 3, 4, 5, 6))
   }
 
-  test("doubleToString") {
+  test("List.doubleToString") {
     val list = Chapter3.List(1.0, 2.0, 3.0, 4.0, 5.0)
     assert(Chapter3.List.doubleToString(list) == Chapter3.List("1.0", "2.0", "3.0", "4.0", "5.0"))
   }
 
-  test("map") {
+  test("List.map") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.map(list)(a => a * 2) == Chapter3.List(2, 4, 6, 8, 10))
   }
 
-  test("filter") {
+  test("List.filter") {
     val list = Chapter3.List(1, 2, 3, 4, 5)
     assert(Chapter3.List.filter(list)(a => a % 2 == 0) == Chapter3.List(2, 4))
   }
 
-  test("flatMap") {
+  test("List.flatMap") {
     val list = Chapter3.List(1, 2, 3)
     assert(Chapter3.List.flatMap(list)(i => Chapter3.List(i, i)) == Chapter3.List(1, 1, 2, 2, 3, 3))
   }
 
-  test("zipWith") {
+  test("List.zipWith") {
     val list1 = Chapter3.List(1, 2, 3)
     val list2 = Chapter3.List(4, 5, 6)
     assert(Chapter3.List.zipWith(list1, list2)((a, b) => a + b) == Chapter3.List(5, 7, 9))
   }
 
-  test("hasSubsequence") {
+  test("List.hasSubsequence") {
     val list = Chapter3.List(1, 2, 3, 4)
     assert(Chapter3.List.hasSubsequence(list, Chapter3.List(1, 2)) == true)
     assert(Chapter3.List.hasSubsequence(list, Chapter3.List(2, 3)) == true)
@@ -118,17 +118,17 @@ class Chapter3Suite extends FunSuite {
     assert(Chapter3.List.hasSubsequence(list, Chapter3.List(5)) == false)
   }
 
-  test("size") {
+  test("Tree.size") {
     val tree = Chapter3.Branch(Chapter3.Leaf(1), Chapter3.Branch(Chapter3.Leaf(2), Chapter3.Leaf(3)))
     assert(Chapter3.Tree.size(tree) == 5)
   }
 
-  test("maximum") {
+  test("Tree.maximum") {
     val tree = Chapter3.Branch(Chapter3.Leaf(5), Chapter3.Branch(Chapter3.Leaf(2), Chapter3.Leaf(3)))
     assert(Chapter3.Tree.maximum(tree) == 5)
   }
 
-  test("depth") {
+  test("Tree.depth") {
     val tree = Chapter3.Branch(Chapter3.Leaf(5), Chapter3.Branch(Chapter3.Leaf(2), Chapter3.Leaf(3)))
     assert(Chapter3.Tree.depth(tree) == 3)
   }
