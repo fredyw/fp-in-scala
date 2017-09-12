0c52fde75cce1a2a09c7f50a9202a375f2d0373d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-09-12 00:08:00 -0500

    Solve Option.getOrElse

--- src/test/scala/org/fredy/fpinscala/Chapter4Suite.scala
+++ src/test/scala/org/fredy/fpinscala/Chapter4Suite.scala
@@ -6,4 +6,9 @@ class Chapter4Suite extends FunSuite {
   test("Option.map") {
     assert(Chapter4.Some("Hello World") == Chapter4.Some("Hello").map(a => a + " World"))
   }
+
+  test("Option.getOrElse") {
+    assert("Hello" == Chapter4.Some("Hello").getOrElse("Bye"))
+    assert("Bye" == Chapter4.None.getOrElse("Bye"))
+  }
 }
