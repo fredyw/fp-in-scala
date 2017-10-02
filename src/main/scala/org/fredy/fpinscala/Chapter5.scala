2dbf92bd42478cc16e59820eeae58ee9c73dcf39
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-02 01:06:12 -0500

    Solve Stream.zipAll

--- src/main/scala/org/fredy/fpinscala/Chapter5.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter5.scala
@@ -92,7 +92,14 @@ object Chapter5 {
     }
 
     def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = {
-      ???
+      Stream.unfold(this, s2)(a => {
+        a match {
+          case (Cons(h1, t1), Empty) => Some(((Some(h1()), None)), (t1(), Stream.empty))
+          case (Empty, Cons(h2, t2)) => Some(((None, Some(h2()))), (Stream.empty, t2()))
+          case (Cons(h1, t1), Cons(h2, t2)) => Some(((Some(h1()), Some(h2()))), (t1(), t2()))
+          case _ => None
+        }
+      })
     }
   }
 
