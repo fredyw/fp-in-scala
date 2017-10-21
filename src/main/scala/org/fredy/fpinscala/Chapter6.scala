021722c37eb01c6c108be9d3de565f358358e389
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-21 10:50:45 -0500

    Skeleton for RNG.flatMap

--- src/main/scala/org/fredy/fpinscala/Chapter6.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter6.scala
@@ -87,5 +87,7 @@ object Chapter6 {
     def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
       fs.foldRight(unit(List[A]()))((a, b) => map2(a, b)((a1, b1) => a1 :: b1))
     }
+
+    def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
   }
 }
