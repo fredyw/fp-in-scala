aacb01f5e272b090cb4ff8cbdbc84ad4d21ed643
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-22 23:35:34 -0500

    Solve RNG._map2

--- src/main/scala/org/fredy/fpinscala/Chapter6.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter6.scala
@@ -107,6 +107,12 @@ object Chapter6 {
       flatMap(s)(a => unit(f(a)))
     }
 
-    def _map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+    def _map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+      flatMap(ra)(a => {
+        map(rb)(b => {
+          f(a, b)
+        })
+      })
+    }
   }
 }
