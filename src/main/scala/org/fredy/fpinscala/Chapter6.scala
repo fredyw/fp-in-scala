e344e6b60cbeb0df0f27e1dcb6dbac7292d12b1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-22 23:28:04 -0500

    Skeleton for RNG._map and RNG._map2

--- src/main/scala/org/fredy/fpinscala/Chapter6.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter6.scala
@@ -102,5 +102,9 @@ object Chapter6 {
         else nonNegativeLessThan(n)
       })
     }
+
+    def _map[A,B](s: Rand[A])(f: A => B): Rand[B] = ???
+
+    def _map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
   }
 }
