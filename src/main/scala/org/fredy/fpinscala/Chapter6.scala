808b916374ac9c4bfea4fa1a8551d2ac279febc0
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-25 20:44:38 -0500

    Skeleton for State case class

--- src/main/scala/org/fredy/fpinscala/Chapter6.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter6.scala
@@ -114,5 +114,13 @@ object Chapter6 {
         })
       })
     }
+
+    case class State[S, +A](run: S => (A, S)) {
+      def map[B](f: A => B): State[S, B] = ???
+
+      def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = ???
+
+      def flatMap[B](f: A => State[S, B]): State[S, B] = ???
+    }
   }
 }
