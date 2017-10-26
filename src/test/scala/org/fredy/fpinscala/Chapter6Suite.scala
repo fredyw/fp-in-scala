797762d1883740b7ecee679cd36d3df3da65432f
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-25 22:08:35 -0500

    Add some skeleton tests for State

--- src/test/scala/org/fredy/fpinscala/Chapter6Suite.scala
+++ src/test/scala/org/fredy/fpinscala/Chapter6Suite.scala
@@ -3,4 +3,15 @@ package org.fredy.fpinscala
 import org.scalatest.FunSuite
 
 class Chapter6Suite extends FunSuite {
+  test("State.map") {
+    // TODO
+  }
+
+  test("State.map2") {
+    // TODO
+  }
+
+  test("State.flatMap") {
+    // TODO
+  }
 }
