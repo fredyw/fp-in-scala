4aedec7611ec9bc5f3a9999d71daedc5eb4ab939
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-25 21:49:24 -0500

    Fix a bug in State.flatMap

--- src/main/scala/org/fredy/fpinscala/Chapter6.scala
+++ src/main/scala/org/fredy/fpinscala/Chapter6.scala
@@ -131,7 +131,7 @@ object Chapter6 {
 
       def flatMap[B](f: A => State[S, B]): State[S, B] = {
         State(s => {
-          val (a, s1) = f(s)
+          val (a, s1) = run(s)
           f(a).run(s1)
         })
       }
