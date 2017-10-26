1912b1692af01bf957d87757b8a639796e871add
Fredy Wijaya <fredy.wijaya@gmail.com>
2017-10-26 00:42:39 -0500

    Write some tests

--- src/test/scala/org/fredy/fpinscala/Chapter6Suite.scala
+++ src/test/scala/org/fredy/fpinscala/Chapter6Suite.scala
@@ -4,14 +4,16 @@ import org.scalatest.FunSuite
 
 class Chapter6Suite extends FunSuite {
   test("State.map") {
-
+    assert(Chapter6.State.unit(1).map(a => a + 1).run("state")._1 == 2)
   }
 
   test("State.map2") {
-    // TODO
+    assert(Chapter6.State.unit(1).map2(Chapter6.State((s: String) => (2, s)))((a, b) => a + b)
+      .run("state")._1 == 3)
   }
 
   test("State.flatMap") {
-    // TODO
+    assert(Chapter6.State((s: String) => (1, s)).flatMap(a => Chapter6.State.unit(a + 1))
+      .run("state")._1 == 2)
   }
 }
